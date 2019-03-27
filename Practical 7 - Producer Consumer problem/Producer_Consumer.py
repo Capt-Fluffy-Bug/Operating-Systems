8e4db7bb963d89a61e07de0e9f4235ec03454c18
Rahul Ramakrishnan <41867601+Capt-Fluffy-Bug@users.noreply.github.com>
2019-03-27 14:10:19 +0530

    Update Producer_Consumer.py

--- Practical 7 - Producer Consumer problem/Producer_Consumer.py
+++ Practical 7 - Producer Consumer problem/Producer_Consumer.py
@@ -5,27 +5,27 @@ import logging
 logging.basicConfig(level=logging.DEBUG,
                     format='(%(threadName)-9s) %(message)s',)
 
-def consumer(cv):
-    with cv:
+def consumer(semaphore):
+    with semaphore:
         logging.debug('Consumer waiting ...\n')
-        cv.wait()
+        semaphore.wait()
         logging.debug('Consumer consumed the resource\n')
 
-def producer(cv):
+def producer(semaphore):
     logging.debug('Producing resources...\n')
-    with cv:
+    with semaphore:
         logging.debug('Making resource available\n')
         logging.debug('Notifying all consumers\n')
-        cv.notifyAll()
+        semaphore.notifyAll()
 
 if __name__ == '__main__':
-    condition = threading.Condition()
-    pd = threading.Thread(name='producer', target=producer, args=(condition,))
-    cs1 = threading.Thread(name='consumer1', target=consumer, args=(condition,))
-    cs2 = threading.Thread(name='consumer2', target=consumer, args=(condition,))
+    condition = threading.Condition() # works as semaphore
+    prod = threading.Thread(name='producer', target=producer, args=(condition,))
+    con1 = threading.Thread(name='consumer1', target=consumer, args=(condition,))
+    con2 = threading.Thread(name='consumer2', target=consumer, args=(condition,))
 
-    cs1.start()
+    con1.start()
     time.sleep(2)
-    cs2.start()
+    con2.start()
     time.sleep(2)
-    pd.start()
+    prod.start()
